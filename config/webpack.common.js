b457af1233532669b649f4ae4deb5b18f408124e
itsBen <benedikt.zneb@gmail.com>
2021-05-29 23:14:20 +0200

    update webpack config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -5,17 +5,56 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const PrettierPlugin = require('prettier-webpack-plugin')
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
-const PreloadWebpackPlugin = require('preload-webpack-plugin')
+// const PreloadWebpackPlugin = require('preload-webpack-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin")
 
 const paths = require('./paths')
 
+const rule = {
+  // JavaScript: Use Babel to transpile JavaScript files
+  js: {
+    test: /\.js$/,
+    use: ['babel-loader']
+  },
+
+  // Images: Copy image files to build folder
+  images: {
+    test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+    type: 'asset/resource'
+  },
+
+  // Fonts and SVGs: Inline files
+  fonts: {
+    test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+    type: 'asset/inline'
+  },
+
+  // Pug files
+  pug: {
+    test: /\.pug$/,
+    use: [
+      {
+        loader: 'html-loader',
+        options: {
+          // Disables attributes processing
+          sources: false,
+        },
+      },
+      {
+        loader: 'pug-html-loader',
+      }
+    ]
+  }
+}
+
 module.exports = {
   // Where webpack looks to start building the bundle
-  entry: [paths.src + '/index.js'],
+  entry: [paths.src + '/app.js'],
 
   // Where webpack outputs the assets and bundles
   output: {
+    assetModuleFilename: 'assets/[name][ext]',
     path: paths.build,
     filename: '[name].bundle.js',
     publicPath: '/',
@@ -40,30 +79,54 @@ module.exports = {
 
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
+    // new HtmlWebpackPlugin({
+    //   title: 'webpack Boilerplate',
+    //   favicon: paths.src + '/images/favicon.ico',
+    //   template: paths.src + '/views/index.html', // template file
+    //   filename: 'index.html', // output file
+    //   inject: 'head',
+    //   'meta': {
+    //     'viewport': 'width=device-width, initial-scale=1',
+    //     'charset': 'utf-8',
+    //     'description' : 'Personal website of Benedikt Benz',
+    //     'keywords':
+    //       'Personal Website, Website, Benedikt Benz, HTML, CSS, JavaScript',
+    //     'author': 'Benedikt Benz'
+    //   }
+    // }),
+
     new HtmlWebpackPlugin({
       title: 'webpack Boilerplate',
-      favicon: paths.src + '/images/favicon.ico',
-      template: paths.src + '/template.html', // template file
-      filename: 'index.html', // output file
+      // favicon: paths.src + '/images/favicon.ico',
+      template: paths.src + '/views/index.pug',
+      filename: 'index.html',
       inject: 'head',
+      'meta': {
+        'viewport': 'width=device-width, initial-scale=1',
+        'charset': 'utf-8',
+        'description' : 'Personal website of Benedikt Benz',
+        'keywords':
+          'Personal Website, Website, Benedikt Benz, HTML, CSS, JavaScript',
+        'author': 'Benedikt Benz'
+      }
     }),
 
-    new HtmlWebpackPlugin({
-      title: '404-page',
-      template: paths.src + '/404.html', // template file
-      filename: '404.html', // output file
-      inject: 'head'
-    }),
+    // new HtmlWebpackPlugin({
+    //   title: '404-page',
+    //   template: paths.src + '/404.html', // template file
+    //   filename: '404.html', // output file
+    //   inject: 'head',
+    // }),
 
-    new PreloadWebpackPlugin({
-      rel: 'preload',
-      as(entry) {
-        if (/\.(woff|woff2|ttf|otf)$/.test(entry)) return 'font'
-        return 'script'
-      },
-      fileWhitelist: [/\.(woff|woff2|ttf|otf)$/],
-      include: 'allAssets'
-    }),
+    // new PreloadWebpackPlugin({
+    //   rel: 'preload',
+    //   as(entry) {
+    //     if (/\.(woff|woff2|ttf|otf)$/.test(entry)) return 'font'
+    //     return 'script'
+    //   },
+    //   fileWhitelist: [/\.(woff|woff2|ttf|otf)$/],
+    //   include: 'allAssets'
+    // }),
 
     new ScriptExtHtmlWebpackPlugin({
       defaultAttribute: 'defer'
@@ -77,29 +140,19 @@ module.exports = {
 
     // Prettier configuration
     new PrettierPlugin(),
+
+    new FaviconsWebpackPlugin({
+      logo: "./src/images/favicon.png",
+      icons: {
+        twitter: true,
+        windows: true,
+      },
+    }),
   ],
 
   // Determine how modules within the project are treated
   module: {
-    rules: [
-      // JavaScript: Use Babel to transpile JavaScript files
-      {
-        test: /\.js$/,
-        use: ['babel-loader']
-      },
-
-      // Images: Copy image files to build folder
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource'
-      },
-
-      // Fonts and SVGs: Inline files
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline'
-      },
-    ],
+    rules: [rule.js, rule.fonts, rule.images, rule.pug],
   },
 
   resolve: {
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,38 +1,22 @@
+const { merge } = require('webpack-merge')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const { merge } = require('webpack-merge')
 
 const paths = require('./paths')
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: 'development',
   devtool: false,
+
+  // Where Webpack outputs the assets and bundles.
   output: {
     path: paths.build,
     publicPath: '/',
     filename: 'js/[name].[contenthash].bundle.js',
   },
-  module: {
-    rules: [
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2,
-              sourceMap: false,
-              modules: true,
-            },
-          },
-          'postcss-loader',
-          'sass-loader',
-        ],
-      },
-    ],
-  },
+
+  // Customize the Webpack build process.
   plugins: [
     // Extracts CSS into separate files
     new MiniCssExtractPlugin({
@@ -51,5 +35,27 @@ module.exports = merge(common, {
     hints: false,
     maxEntrypointSize: 512000,
     maxAssetSize: 512000,
+  },
+
+  // Determine how modules within the project are treated.
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: false,
+              // modules: true,
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
   },
 })
